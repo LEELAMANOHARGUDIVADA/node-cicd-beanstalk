263129c3cfbe60e308542b8d1734add69ef75c35
Leela Manohar Gudivada <mgudivada@msa.miraclesoft.com>
2025-08-07 00:35:16 +0530

    Enhanced Search Filter Endpoint for Multi Search

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -48,67 +48,167 @@ const searchCustomers = async (req, res) => {
         let conditions = [];
 
         if (BA_Origin && BA_Origin != "") {
-            conditions.push(`LOWER(BA_ORIGIN) = LOWER('${BA_Origin}')`);
+            const queries = BA_Origin.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(BA_ORIGIN) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
 
         if (Customer_Group_Calc && Customer_Group_Calc != "") {
-            conditions.push(`LOWER(Customer_Group_Calc) = LOWER('${Customer_Group_Calc}')`);
+            const queries = Customer_Group_Calc.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Customer_Group_Calc) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Cust_Name && Cust_Name != "") {
-            conditions.push(`LOWER(Cust_Name) = LOWER('${Cust_Name}')`);
+            const queries = Cust_Name.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Cust_Name) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Cust_No && Cust_No != "") {
-            conditions.push(`LOWER(Cust_No) = ('${Cust_No}')`);
+            const queries = Cust_No.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Cust_No) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Cust_Elim_or_Name && Cust_Elim_or_Name != "") {
-            conditions.push(`LOWER(Cust_Elim_or_Name) = LOWER('${Cust_Elim_or_Name}')`);
+            const queries = Cust_Elim_or_Name.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Cust_Elim_or_Name) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Cust_Sales_Area && Cust_Sales_Area != "") {
-            conditions.push(`LOWER(Cust_Sales_Area) = LOWER('${Cust_Sales_Area}')`);
+            const queries = Cust_Sales_Area.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Cust_Sales_Area) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
-        if (Operational_Hub_Code && Cust_Sales_Area != "") {
-            conditions.push(`LOWER(Operational_Hub_Code) = LOWER('${Operational_Hub_Code}')`);
+        if (Operational_Hub_Code && Operational_Hub_Code != "") {
+            const queries = Operational_Hub_Code.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Operational_Hub_Code) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
 
         if (Company_Code && Company_Code != "") {
-            conditions.push(`LOWER(Company_Code) = LOWER('${Company_Code}')`);
+            const queries = Company_Code.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Company_Code) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Company_Full_Name && Company_Full_Name != "") {
-            conditions.push(`LOWER(Company_Full_Name) = LOWER('${Company_Full_Name}')`);
+            const queries = Company_Full_Name.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Company_Full_Name) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
 
         if (Segment && Segment != "") {
-            conditions.push(`LOWER(Segment) = LOWER('${Segment}')`)
+            const queries = Segment.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Segment) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Sales_Person_Name && Sales_Person_Name != "") {
-            conditions.push(`LOWER(Sales_Person_Name) = LOWER('${Sales_Person_Name}')`)
+            const queries = Sales_Person_Name.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Sales_Person_Name) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Market_Code && Market_Code != "") {
-            conditions.push(`LOWER(Market_Code) = LOWER('${Market_Code}')`)
+            const queries = Market_Code.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Market_Code) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Operational_Site_Group && Operational_Site_Group != "") {
-            conditions.push(`LOWER(Operational_Site_Group) = LOWER('${Operational_Site_Group}')`)
+            const queries = Operational_Site_Group.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Operational_Site_Group) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Plant_Code && Plant_Code != "") {
-            conditions.push(`LOWER(Plant_Code) = LOWER('${Plant_Code}')`)
+            const queries = Plant_Code.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Plant_Code) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Plant_Name && Plant_Name != "") {
-            conditions.push(`LOWER(Plant_Name) = LOWER('${Plant_Name}')`)
+            const queries = Plant_Name.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Plant_Name) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Commercial_Name && Commercial_Name != "") {
-            conditions.push(`LOWER(Commercial_Name) = LOWER('${Commercial_Name}')`)
+            const queries = Commercial_Name.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Commercial_Name) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Package_Type && Package_Type != "") {
-            conditions.push(`LOWER(Package_Type) = LOWER('${Package_Type}')`)
+            const queries = Package_Type.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Package_Type) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Product_Code && Product_Code != "") {
-            conditions.push(`LOWER(Product_Code) = LOWER('${Product_Code}')`)
+            const queries = Product_Code.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Product_Code) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (Mineral_Calc && Mineral_Calc != "") {
-            conditions.push(`LOWER(Mineral_Calc) = LOWER('${Mineral_Calc}')`)
+            const queries = Mineral_Calc.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(Mineral_Calc) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
         if (SOP_Mineral_Group && SOP_Mineral_Group != "") {
-            conditions.push(`LOWER(SOP_Mineral_Group) = LOWER('${SOP_Mineral_Group}')`)
+            const queries = SOP_Mineral_Group.split(',');
+            let multiquery = [];
+            for(let i = 0; i<queries.length; i++){
+                multiquery.push(`LOWER(SOP_Mineral_Group) LIKE LOWER('%${queries[i]}%')`)
+            }
+            conditions.push(`${multiquery.join(' OR ')}`);
         }
 
         if(conditions.length == 0){
