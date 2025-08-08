1cb2ac9fab534683901aa17096ff6d5bd0b3c966
Leela Manohar Gudivada <mgudivada@msa.miraclesoft.com>
2025-08-08 17:47:44 +0530

    Update Get All Customers Query

--- constants/queries.js
+++ constants/queries.js
@@ -1,3 +1,3 @@
-export const allCustomersQuery = "SELECT * FROM customer_data";
+export const allCustomersQuery = "SELECT * FROM customer_data ORDER BY id";
 export const searchCustomersQuery = "SELECT * FROM customer_data WHERE "
 export const updateCustomersQuery = "UPDATE customer_data SET "
